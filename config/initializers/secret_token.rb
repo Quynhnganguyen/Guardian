8b070064d5017d3d5ca27586a1252052ea1090f5
Quynhnga <quynhnga.k59spt@gmail.com>
2013-04-04 10:17:39 +0700

    firstcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuardianService::Application.config.secret_token = 'cc293309404700586c7d6718b53db11977926a003f40dec67913feee437115b150908c2c06dbbc393f57ac2b3d1313d6277bc2923437c6e6abc00f90717ddbbe'
